9a6a93d6a2f888b0313893681f32623e2f4d20b2
Jahn Saito <jahn.saito@gmail.com>
2026-03-24 08:30:36 +0000

    Allow terraform-apply to be assumed cross acount

--- terraform/iam/role/terraform-apply.tf
+++ terraform/iam/role/terraform-apply.tf
@@ -7,7 +7,7 @@ resource "aws_iam_role" "terraform-apply" {
     - https://docs.github.com/en/actions/how-tos/secure-your-work/security-harden-deployments/oidc-in-aws#configuring-the-role-and-trust-policy
   EOT
 
-  assume_role_policy = data.aws_iam_policy_document.oidc-provider-github-assume-role-with-web-idenity.json
+  assume_role_policy = data.aws_iam_policy_document.terraform-apply-assume-role-combined.json
 
   max_session_duration = 3600 # 1 hour
 }
@@ -16,6 +16,28 @@ resource "aws_iam_role_policy" "gh-oidc-cloud-nova-crop-terraform-apply-s3-bucke
   role   = aws_iam_role.terraform-apply.name
   name   = "s3-bucket-cloud-nova-corp-terraform-fullaccess"
   policy = data.aws_iam_policy_document.s3-bucket-cloud-nova-corp-terraform-fullaccess.json
+}
+
+# allow production account apply role to access bucket
+data "aws_iam_policy_document" "cross-account-assume-role-production-fullaccess" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "AWS"
+
+      identifiers = [
+	"arn:aws:iam::205899621967:user/cloud-nova-corp-terraform" # apply user
+      ]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "terraform-apply-assume-role-combined" {
+  source_policy_documents = [
+    data.aws_iam_policy_document.oidc-provider-github-assume-role-with-web-idenity.json,
+    data.aws_iam_policy_document.cross-account-assume-role-production-fullaccess.json
+  ]
 }
 
 output "iam-role-terraform-apply" {
--- terraform/secretsmanager/terraform-github-access-token.tf
+++ terraform/secretsmanager/terraform-github-access-token.tf
@@ -4,7 +4,7 @@ resource "aws_secretsmanager_secret" "terraform_github_access_token" {
   description = <<EOS
 Access token used for applying changes with Terraform provider GitHub in the GitHub organization cloud-nova-crop.
 
-The value is managed manually.
+The value is managed manually. 
 
 The token is a classic personal access token of GitHub user tf with reopsitory read/write scope.
 EOS
