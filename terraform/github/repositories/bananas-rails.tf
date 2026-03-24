c8333dd5d4520a84090f1b0caa6317e56358bae1
Jahn Saito <jahn.saito@gmail.com>
2026-03-24 11:48:59 +0000

    Ignore recurring diff

---
+++ terraform/github/repositories/bananas-rails.tf
@@ -0,0 +1,141 @@
+resource "github_repository" "bananas-rails" {
+  name        = "bananas.rails"
+  description = "Bananas Rails apps. This environment specific but currently only production."
+  topics      = ["rails"]
+
+  visibility = "public"
+
+  has_issues   = false
+  has_projects = false
+  has_wiki     = false
+
+  auto_init              = true
+  is_template            = false
+  delete_branch_on_merge = true
+
+  vulnerability_alerts = false
+}
+
+resource "github_branch" "bananas-rails-main" {
+  repository = github_repository.bananas-rails.name
+  branch     = "main"
+}
+
+resource "github_branch_default" "bananas-rails-default" {
+  repository = github_repository.bananas-rails.name
+  branch     = github_branch.bananas-rails-main.branch
+}
+
+resource "github_branch_protection" "bananas-rails-default" {
+  repository_id = github_repository.bananas-rails.name
+
+  pattern          = "main"
+  enforce_admins   = false
+  allows_deletions = true
+}
+
+
+## repository environment production for Terraform
+resource "github_repository_environment" "bananas-rails-production" {
+  repository  = github_repository.bananas-rails.name
+  environment = "production"
+
+  reviewers {
+    users = [
+      data.github_user.gkameari.id,
+      data.github_user.jtsaito.id,
+    ]
+  }
+
+  lifecycle {
+    # known provider bug, manage manually
+    ignore_changes = [reviewers]
+  }
+}
+
+resource "github_actions_environment_variable" "bananas-rails-production-aws-iam-role-terraform-apply" {
+  repository  = github_repository.bananas-rails.name
+  environment = github_repository_environment.bananas-rails-production.environment
+
+  variable_name = "AWS_IAM_ROLE_TERRAFORM_APPLY"
+  value         = aws_iam_role.terraform-apply.arn
+}
+
+resource "github_actions_environment_variable" "bananas-rails-production-environment" {
+  repository  = github_repository.bananas-rails.name
+  environment = github_repository_environment.bananas-rails-production.environment
+
+  variable_name = "ENVIRONMENT"
+  value         = "production"
+}
+
+resource "github_actions_environment_variable" "bananas-rails-production-aws-region-terraform-apply" {
+  repository  = github_repository.bananas-rails.name
+  environment = github_repository_environment.bananas-rails-production.environment
+
+  variable_name = "AWS_REGION_TERRAFORM_APPLY"
+  value         = data.aws_region.current.region
+}
+
+resource "github_actions_environment_variable" "bananas-rails-production-tf-backend" {
+  for_each = {
+    BUCKET   = aws_s3_bucket.cloud-nova-corp-terraform.bucket
+    REGION   = data.aws_region.current.region
+    ROLE_ARN = data.aws_iam_role.fullaccess.arn
+  }
+
+  repository  = github_repository.bananas-rails.name
+  environment = github_repository_environment.bananas-rails-production.environment
+
+  variable_name = "TF_BACKEND_${each.key}"
+  value         = each.value
+}
+
+resource "github_actions_environment_variable" "bananas-rails-productin-tf-backend-key" {
+  repository  = github_repository.bananas-rails.name
+  environment = github_repository_environment.bananas-rails-production.environment
+
+  variable_name = "TF_BACKEND_KEY"
+  value         = "${github_repository.bananas-rails.name}/production/terraform.tfstate"
+}
+
+
+## bananas ECR push
+
+resource "github_repository_environment" "bananas-rails-ecr-push" {
+  repository  = github_repository.bananas-rails.name
+  environment = "bananas-ecr-push"
+
+  reviewers {
+    teams = [data.github_user.jtsaito.id]
+  }
+
+  lifecycle {
+    # known provider bug, manage manually
+    ignore_changes = [reviewers]
+  }
+}
+
+resource "github_actions_environment_variable" "bananas-rails-ecr-push-aws-iam-role-arn-bananas-deploy" {
+  repository  = github_repository.bananas-rails.name
+  environment = github_repository_environment.bananas-rails-ecr-push.environment
+
+  variable_name = "AWS_IAM_ROLE_ARN_BANANAS_DEPLOY"
+  value         = aws_iam_role.bananas-deploy.arn
+}
+
+resource "github_actions_environment_variable" "bananas-rails-ecr-push-aws-region-bananas-deploy" {
+  repository  = github_repository.bananas-rails.name
+  environment = github_repository_environment.bananas-rails-ecr-push.environment
+
+  variable_name = "AWS_IAM_REGION_BANANAS_DEPLOY"
+  value         = "eu-west-1" # should be derived from ECR
+}
+
+resource "github_actions_environment_variable" "bananas-rails-ecr-push-ecr-repository-bananas" {
+  repository  = github_repository.bananas-rails.name
+  environment = github_repository_environment.bananas-rails-ecr-push.environment
+
+  variable_name = "AWS_ECR_REPOSITORY_BANANAS"
+  value         = module.ecr-repository-bananas.repository.name
+}
